9e92f350c9351823e2dfc0c051b151e3dd4dceef
edoardomanieri <edoardomanieri@gmail.com>
2020-04-26 17:30:09 +0200

    modify tests

--- matches_predictor/tests/test_goals.py
+++ matches_predictor/tests/test_goals.py
@@ -3,8 +3,8 @@ from matches_predictor import goals
 
 def test_not_empty_df():
     df = goals.get_live_predictions(
-        clf='xgb', reprocess_train_data=True, retrain_model=True, res_path='../../res/csv_test')
+        reprocess=True, retrain=True, res_path='../../res/csv_test')
     assert len(df) != 0
     df = goals.get_live_predictions(
-        clf='xgb', reprocess_train_data=False, retrain_model=False, res_path='../../res/csv_test')
+        reprocess=False, retrain=False, res_path='../../res/csv_test')
     assert len(df) != 0
